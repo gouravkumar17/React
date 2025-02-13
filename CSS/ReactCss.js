c3ba7c6b3ec839334b3f44d088bf97ee40e6cd36
Gourav Kumar <gourav.kumar@lpu.in>
2025-02-13 23:09:26 +0530

    Add files via upload

---
+++ CSS/ReactCss.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './ReactCss.css'
+function ReactCss(){
+    return(
+        <div>
+            <h1 className="heading">Component 1</h1>
+            <p className="para">I am a Btech Student</p>
+        </div>
+    )
+}
+export default ReactCss;
